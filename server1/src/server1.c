fccc85731cf1c19b95d13acd95fdc7830aed876b
Ki Sung Lim <kisungqueso@hotmail.com>
2020-11-29 11:45:47 -0600

    [Update] Seq-fork servers working/tested, no file transfer working
    BUG* the file transfer or image isn't working properly and is making the servers don't iterate with the same client's socket.

--- server1/src/server1.c
+++ server1/src/server1.c
@@ -17,21 +17,28 @@
 #include "sobel.h"
 #include "file_operations.h"
 
-#define MAX 80 
-// #define PORT 9090
+#define MAX 80
 #define TRUE   1  
 #define FALSE  0
 #define NO_INPUT 2
-#define R 2
-#define G 3
-#define B 4
 #define SA struct sockaddr 
 #define NET_BUF_SIZE 32 
+#define ENOUGH ((CHAR_BIT * sizeof(int) - 1) / 3 + 2)
+
+union LongChar {
+    char myByte[4];
+    long mylong;
+};
 
 // int readcolor(char *image);
 
 // Function designed for file(picture) transfer between client and server. 
 void func(int sockfd);
+
+// Read
+int buff_read(int sockfd, char buff[MAX]);
+
+int receive_image(int sockfd);
 
 // Driver function 
 int main(int argc , char *argv[]) 
@@ -72,17 +79,29 @@ int main(int argc , char *argv[])
 		printf("Socket successfully binded..\n"); 
 
 	// Now server is ready to listen and verification 
-	if ((listen(sockfd, 5)) != 0) { 
+	if ((listen(sockfd, 10)) != 0) { 
 		printf("ERROR: Listen failed...\n"); 
 		exit(1); 
 	} 
 
+    len = sizeof(cli); 
+    
+    // char *client_ip = inet_ntoa(cli.sin_addr);
+    
+    // func(connfd);
+
+    char buff[MAX+1];
+    char *name;
+    char *msg_rcv = "Image processed";
+	int n;
+    FILE *image;
+    int first = 0;
+    int counter = 0;
+    ssize_t nread;
+
     while (TRUE)
     {
         printf("Server listening..\n"); 
-    
-        len = sizeof(cli); 
-
         // Accept the data packet from client and verification 
         connfd = accept(sockfd, (SA*)&cli, &len); 
         if (connfd < 0) { 
@@ -90,215 +109,116 @@ int main(int argc , char *argv[])
             exit(0); 
         } 
         else
-            printf("server acccept the client...\n"); 
+            printf("server acccept the client...\n");
 
-        // Last project ips acceptance configuration
-        // 
-        // const char* trusted = getenv("TRUSTED_HOSTS");
-        // const char* not_trusted = getenv("NOT_TRUSTED_HOSTS");
+        //Somebody connected , get his details and print  
+        getpeername(connfd, (SA*)&cli,(socklen_t*)&len); 
 
-        // printf("Are the ips ok?\n");
-        // if (trusted == NULL || trusted == "" || not_trusted == NULL) {
-        //     printf("Didn't get the ips correctly.\n");
-        //     exit(1);
+        printf("Host connected , ip %s , port %d \n" , 
+            inet_ntoa(cli.sin_addr) , ntohs(cli.sin_port));
+
+        while (TRUE) {
+
+            char temp_dir[100] = "../imgs/";
+
+            //Read Picture Size
+            printf("Reading Picture Size\n");
+            bzero(buff, sizeof(buff));
+
+            long size;
+            if (read(connfd, &size, sizeof(long)) == 0) {
+                break;
+            }
+
+            // if ((strncmp(buff, "exit", 5)) == 0) { 
+            //     printf("Client Exit...\n"); 
+            //     exit(0); 
+            // }
+
+            // union LongChar myUnion;
+            // myUnion.myByte[0] = buff[0];
+            // myUnion.myByte[1] = buff[1];
+            // myUnion.myByte[2] = buff[2];
+            // myUnion.myByte[3] = buff[3];
+
+
+            // long size = myUnion.mylong;
+            printf("Size: %ld\n", size);
+
+
+            //Read Picture Name
+            printf("Reading Picture Name\n");
+            bzero(buff, sizeof(buff));
+            
+            if (read(connfd, buff, MAX) == 0) {
+                break;
+            }
+
+            char *token = strtok(buff, "/");
+
+            do {   
+                name = token;
+                token = strtok(NULL, "/");
+            } while (token != NULL);
+
+            printf("Name: %s\n", name);
+
+            // break;
+            char counter_str[4];
+            sprintf(counter_str, "%d", counter);
+            strcat(temp_dir, counter_str);
+            strcat(temp_dir, name);
+            strcat(temp_dir,"\0");
+            printf("%s\n",temp_dir);
+            
+            
+            //Read Picture Byte Array
+            printf("Reading Picture Byte Array\n");
+            char p_array[size+1];
+            if((nread = read(connfd, p_array, size)) == 0) {
+                fclose(image);
+                break;
+            }
+            if (counter < 100) {
+                image = fopen(temp_dir, "w");
+
+                if (image == NULL) {
+                    printf("Image didn't open\n");
+                    exit(0);
+                }
+
+
+                fwrite(p_array, 1, size, image); 
+                fclose(image);
+            }
+            // long counter = 0L;
+            // while(counter < size) {
+            //     bzero(buff, sizeof(buff));
+            //     ssize_t nread = read(connfd, buff, 256);
+            //     fwrite(buff, 1, nread, image);
+            //     counter += sizeof(buff);
+            // }
+
+            printf("Responding to client\n");
+            bzero(buff, sizeof(buff)); 
+            write(connfd, msg_rcv, MAX);
+            
+            if (counter < 100) {
+                counter ++;
+            }
+
+        }
+
+        // printf("Confirming zeroes\n");
+        // while ((nread = read(sockfd, p_array, MAX)) != 0) {
+
         // }
 
-        // printf("%s\n", trusted);
-        char *client_ip = inet_ntoa(cli.sin_addr);
-
-        // char *istrusted = strstr(trusted, client_ip);
-        // char *isnottrusted = strstr(not_trusted, client_ip);
-
-        // printf("%s\n", client_ip);
-
-        // if (istrusted != NULL && isnottrusted == NULL) {
-        //     func(connfd, 0);
-        // } else if (istrusted == NULL && isnottrusted != NULL) {
-        //     func(connfd, 1);
-        // } else {
-        //     printf("Rejected: ip %s\n", client_ip);
-        // }
-        func(connfd);
-
-
-
+        // break; 
+        close(connfd);
     }
+
     close(sockfd);
-}
 
-// int readcolor(char *image) {
-  
-//     FILE *fp;
-//     char path[100] = "convert ";
-//     char msg[20];
-//     strcat(path, image);
-//     strcat(path, " -scale 1x1\\! -format '%[pixel:s]\n' info:-");
-
-//     /* Open the command for reading. */
-//     printf("%s\n", path);
-//     fp = popen(path, "r");
-//     if (fp == NULL) {
-//         printf("Failed to run command\n" );
-//         exit(1);
-//     }
-
-//     /* Read the output a line at a time - output it. */
-//     // while (fgets(msg, sizeof(msg), fp) != NULL) {
-//     //     printf("%s\n", msg);
-//     // }
-
-//     if (fgets(msg, sizeof(msg), fp) == NULL) {
-//         printf("Bad reading in cmd\n");
-//         exit(1);
-//     }
-
-//     /* close */
-//     pclose(fp);
-
-//     // Function to convert a string to
-//     // integer array
-//     int arr[3]; 
-  
-//     int j = 0, i, sum = 0; 
-  
-//     // Traverse the string 
-//     for (i = 0; msg[i] != '\0'; i++) { 
-        
-//         if (j >= 3) {
-//             break;
-//         }
-
-//         if (i < 5)
-//             continue;
-//         // if str[i] is ', ' then split 
-//         else if (msg[i] == ',' || msg[i] == ')') {
-//             arr[j] = sum;
-//             sum = 0;
-//             j++;
-//         }
-//         // ignore others char
-//         else if (msg[i] == ' ' || msg[i] == '(' || msg[i] == 'b' || msg[i] == '.')
-//             continue;
-//         else
-//             // subtract str[i] by 48 to convert it to int 
-//             // Generate number by multiplying 10 and adding 
-//             // (int)(str[i]) 
-//             sum = sum * 10 + (msg[i] - '0'); 
-//     } 
-    
-//     // printf("R:%d G:%d B:%d\n", arr[0], arr[1], arr[2]);
-
-//     if (arr[0] > arr[1] && arr[0] > arr[2]) {
-//         return R;
-//     } else if (arr[1] > arr[2]) {
-//         return G;
-//     } else {
-//         return B;
-//     }
-
-//     // return 0;
-// }
-
-void func(int sockfd)
-{ 
-	char buff[MAX];
-    char *name;
-	int n;
-    FILE *image;
-
-    // char n_dir[100] = "/volume/Not_trusted/";
-    // char r_dir[100] = "/volume/R/";
-    // char g_dir[100] = "/volume/G/";
-    // char b_dir[100] = "/volume/B/";
-    char temp_dir[100] = "../imgs/";
-    char *msg_rcv = "Image processed!\0";
-
-    //Read Picture Size
-    printf("Reading Picture Size\n");
-    int size;
-    // bzero(size, sizeof(size));
-    read(sockfd, &size, sizeof(int));
-
-    //Read Picture Name
-    printf("Reading Picture Name\n");
-    bzero(buff, sizeof(buff));
-    read(sockfd, buff, sizeof(buff));
-
-    char *token = strtok(buff, "/");
-
-    do {   
-        name = token;
-        token = strtok(NULL, "/");
-    } while (token != NULL);
-
-    //Read Picture Byte Array
-    printf("Reading Picture Byte Array\n");
-    char p_array[size];
-    bzero(p_array, sizeof(p_array));
-    read(sockfd, p_array, size);
-
-    // if (type) {
-    //     //Convert it Back into Picture
-    //     printf("Converting Byte Array to Picture Not Trusted\n");
-    //     strcat(n_dir, name);
-    //     strcat(n_dir,"\0");
-    //     printf("%s\n",n_dir);
-    //     image = fopen(n_dir, "w");
-
-    //     if (image == NULL) {
-    //         printf("Image didn't open\n");
-    //         exit(0);
-    //     }
-    //     fwrite(p_array, 1, sizeof(p_array), image);
-    //     fclose(image);
-    // } else {
-        //Convert it Back into Picture
-    printf("Converting Byte Array to Picture Trusted\n");
-
-    strcat(temp_dir, name);
-    strcat(temp_dir,"\0");
-    printf("%s\n",temp_dir);
-    image = fopen(temp_dir, "w");
-
-    if (image == NULL) {
-        printf("Image didn't open\n");
-        exit(0);
-    }
-    fwrite(p_array, 1, sizeof(p_array), image);
-    fclose(image);
-
-    // bzero(buff, sizeof(buff)); 
-    write(sockfd, msg_rcv, sizeof(msg_rcv));
-
-    /*int cond = readcolor(temp_dir);
-
-    int del = remove(temp_dir);
-    if (!del)
-        printf("The file is Deleted successfully\n");
-    else {
-        printf("the file is not Deleted\n");
-        exit(1);
-    }
-
-    if (cond == R) {
-        strcat(r_dir, name);
-        strcat(r_dir,"\0");
-        image = fopen(r_dir, "w");
-    } else if (cond == G) {
-        strcat(g_dir, name);
-        strcat(g_dir,"\0");
-        image = fopen(g_dir, "w");
-    } else {
-        strcat(b_dir, name);
-        strcat(b_dir,"\0");
-        image = fopen(b_dir, "w");
-    }
-
-    if (image == NULL) {
-        printf("Image didn't open\n");
-        exit(0);
-    }
-    fwrite(p_array, 1, sizeof(p_array), image);
-    fclose(image);*/
-    // }
+    return 0;
 }
--- server2/server2.c
+++ server2/server2.c
@@ -1,193 +1,48 @@
 #include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>  
+#include <sys/types.h>
+
+#include <unistd.h>   //file close
+#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros
+
 #include <netdb.h> 
 #include <netinet/in.h> 
-#include <stdlib.h> 
-#include <string.h> 
 #include <sys/socket.h> 
-#include <sys/types.h>
-#include <errno.h>  
-#include <unistd.h>   //close  
-#include <arpa/inet.h>    //close   
-#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros  
-#define MAX 80 
-#define PORT 9090
+#include <arpa/inet.h>    //socket close     
+
+// For image processing
+// #include "macros.h"
+// #include "sobel.h"
+// #include "file_operations.h"
+
+#define MAX 80
 #define TRUE   1  
-#define FALSE  0 
-#define R 2
-#define G 3
-#define B 4
+#define FALSE  0
+#define NO_INPUT 2
 #define SA struct sockaddr 
 #define NET_BUF_SIZE 32 
+#define ENOUGH ((CHAR_BIT * sizeof(int) - 1) / 3 + 2)
 
-int readcolor(char *image) {
-  
-    FILE *fp;
-    char path[100] = "convert ";
-    char msg[20];
-    strcat(path, image);
-    strcat(path, " -scale 1x1\\! -format '%[pixel:s]\n' info:-");
+union LongChar {
+    char myByte[4];
+    long mylong;
+};
 
-    /* Open the command for reading. */
-    printf("%s\n", path);
-    fp = popen(path, "r");
-    if (fp == NULL) {
-        printf("Failed to run command\n" );
-        exit(1);
-    }
-
-    /* Read the output a line at a time - output it. */
-    // while (fgets(msg, sizeof(msg), fp) != NULL) {
-    //     printf("%s\n", msg);
-    // }
-
-    if (fgets(msg, sizeof(msg), fp) == NULL) {
-        printf("Bad reading in cmd\n");
-        exit(1);
-    }
-
-    /* close */
-    pclose(fp);
-
-    // Function to convert a string to
-    // integer array
-    int arr[3]; 
-  
-    int j = 0, i, sum = 0; 
-  
-    // Traverse the string 
-    for (i = 0; msg[i] != '\0'; i++) { 
-        
-        if (j >= 3) {
-            break;
-        }
-
-        if (i < 5)
-            continue;
-        // if str[i] is ', ' then split 
-        else if (msg[i] == ',' || msg[i] == ')') {
-            arr[j] = sum;
-            sum = 0;
-            j++;
-        }
-        // ignore others char
-        else if (msg[i] == ' ' || msg[i] == '(' || msg[i] == 'b' || msg[i] == '.')
-            continue;
-        else
-            // subtract str[i] by 48 to convert it to int 
-            // Generate number by multiplying 10 and adding 
-            // (int)(str[i]) 
-            sum = sum * 10 + (msg[i] - '0'); 
-    } 
-    
-    // printf("R:%d G:%d B:%d\n", arr[0], arr[1], arr[2]);
-
-    if (arr[0] > arr[1] && arr[0] > arr[2]) {
-        return R;
-    } else if (arr[1] > arr[2]) {
-        return G;
-    } else {
-        return B;
-    }
-
-    // return 0;
-}
+// int readcolor(char *image);
 
 // Function designed for file(picture) transfer between client and server. 
-void func(int sockfd, int type) 
-{ 
-	char buff[MAX];
-    char *name;
-	int n;
-    FILE *image;
+void func(int sockfd);
 
-    char n_dir[100] = "/volume/Not_trusted/";
-    char r_dir[100] = "/volume/R/";
-    char g_dir[100] = "/volume/G/";
-    char b_dir[100] = "/volume/B/";
-    char temp_dir[100] = "/volume/temp/";
+// Read
+int buff_read(int sockfd, char buff[MAX]);
 
-    //Read Picture Size
-    printf("Reading Picture Size\n");
-    int size;
-    read(sockfd, &size, sizeof(int));
+int receive_image(int sockfd);
 
-    //Read Picture Name
-    printf("Reading Picture Name\n");
-    read(sockfd, buff, sizeof(buff));
-
-    char *token = strtok(buff, "/");
-
-    do {   
-        name = token;
-        token = strtok(NULL, "/");
-    } while (token != NULL);
-
-    //Read Picture Byte Array
-    printf("Reading Picture Byte Array\n");
-    char p_array[size];
-    read(sockfd, p_array, size);
-
-    if (type) {
-        //Convert it Back into Picture
-        printf("Converting Byte Array to Picture Not Trusted\n");
-        strcat(n_dir, name);
-        strcat(n_dir,"\0");
-        printf("%s\n",n_dir);
-        image = fopen(n_dir, "w");
-
-        if (image == NULL) {
-            printf("Image didn't open\n");
-            exit(0);
-        }
-        fwrite(p_array, 1, sizeof(p_array), image);
-        fclose(image);
-    } else {
-        //Convert it Back into Picture
-        printf("Converting Byte Array to Picture Trusted\n");
-
-        strcat(temp_dir, name);
-        strcat(temp_dir,"\0");
-        printf("%s\n",temp_dir);
-        image = fopen(temp_dir, "w");
-
-        if (image == NULL) {
-            printf("Image didn't open\n");
-            exit(0);
-        }
-        fwrite(p_array, 1, sizeof(p_array), image);
-        fclose(image);
-
-        int cond = readcolor(temp_dir);
-
-        int del = remove(temp_dir);
-        if (!del)
-            printf("The file is Deleted successfully\n");
-        else {
-            printf("the file is not Deleted\n");
-            exit(1);
-        }
-
-        if (cond == R) {
-            strcat(r_dir, name);
-            strcat(r_dir,"\0");
-            image = fopen(r_dir, "w");
-        } else if (cond == G) {
-            strcat(g_dir, name);
-            strcat(g_dir,"\0");
-            image = fopen(g_dir, "w");
-        } else {
-            strcat(b_dir, name);
-            strcat(b_dir,"\0");
-            image = fopen(b_dir, "w");
-        }
-
-        if (image == NULL) {
-            printf("Image didn't open\n");
-            exit(0);
-        }
-        fwrite(p_array, 1, sizeof(p_array), image);
-        fclose(image);
-    }
+void error(const char *msg) {
+    perror(msg);
+    exit(1);
 }
 
 // Driver function 
@@ -199,11 +54,16 @@ int main(int argc , char *argv[])
     // int addrlen = sizeof(servaddr);
     // char net_buf[NET_BUF_SIZE]; 
 
+    if (argc != NO_INPUT) {
+        printf("ERROR: num of param #%d, param needed #%d\n", argc, NO_INPUT);
+        exit(1);
+    }
+
 	// socket create and verification 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
 	if (sockfd == -1) { 
-		printf("socket creation failed...\n"); 
-		exit(0); 
+		printf("ERROR: Socket creation failed...\n"); 
+		exit(1); 
 	} 
 	else
 		printf("Socket successfully created..\n"); 
@@ -213,62 +73,246 @@ int main(int argc , char *argv[])
     // assign IP, PORT 
 	servaddr.sin_family = AF_INET; 
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-	servaddr.sin_port = htons(PORT); 
+	servaddr.sin_port =htons(atoi(argv[1]));
 
 	// Binding newly created socket to given IP and verification 
 	if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
-		printf("socket bind failed...\n"); 
-		exit(0); 
+		printf("ERROR: Socket bind failed...\n"); 
+		exit(1); 
 	} 
 	else
 		printf("Socket successfully binded..\n"); 
 
 	// Now server is ready to listen and verification 
-	if ((listen(sockfd, 5)) != 0) { 
-		printf("Listen failed...\n"); 
-		exit(0); 
+	if ((listen(sockfd, 10)) != 0) { 
+		printf("ERROR: Listen failed...\n"); 
+		exit(1); 
 	} 
-	else
-        while (TRUE)
-        {
-            printf("Server listening..\n"); 
-	    
-            len = sizeof(cli); 
 
-            // Accept the data packet from client and verification 
-            connfd = accept(sockfd, (SA*)&cli, &len); 
-            if (connfd < 0) { 
-                printf("server acccept failed...\n"); 
-                exit(0); 
-            } 
-            else
-                printf("server acccept the client...\n"); 
+    len = sizeof(cli); 
+    
+    // char *client_ip = inet_ntoa(cli.sin_addr);
+    
+    // func(connfd);
 
-            const char* trusted = getenv("TRUSTED_HOSTS");
-            const char* not_trusted = getenv("NOT_TRUSTED_HOSTS");
+    char buff[MAX+1];
+    char *name;
+    char *msg_rcv = "Image processed";
+	int n;
+    FILE *image;
+    int first = 0;
+    int counter = 0;
+    ssize_t nread;
 
-            printf("Are the ips ok?\n");
-            if (trusted == NULL || trusted == "" || not_trusted == NULL) {
-                printf("Didn't get the ips correctly.\n");
-                exit(1);
+    int pid;
+    // while (1) {
+    //      newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    //      if (newsockfd < 0)
+    //             error("ERROR on accept");
+    //      //fork new process
+    //      pid = fork();
+    //      if (pid < 0) {
+    //           error("ERROR in new process creation");
+    //      }
+    //      if (pid == 0) {
+    //         //child process
+    //         close(sockfd);
+    //         //do whatever you want
+    //         bzero(buffer, 256);
+    //         n = read(newsockfd, buffer, 255);
+    //         if (n < 0)
+    //             error("ERROR reading from socket");
+    //         printf("Here is the message: %s\n", buffer);
+    //         n = write(newsockfd, "I got your message", 18);
+    //         if (n < 0)
+    //              error("ERROR writing to socket");
+    //         close(newsockfd);
+    //       } else {
+    //          //parent process
+    //          close(newsockfd);
+    //       }
+    // }
+
+    while (TRUE)
+    {
+        printf("Server listening..\n"); 
+        // Accept the data packet from client and verification 
+        connfd = accept(sockfd, (SA*)&cli, &len); 
+        if (connfd < 0) { 
+            printf("server acccept failed...\n"); 
+            exit(0); 
+        } 
+        else
+            printf("server acccept the client...\n");
+
+        //fork new process
+        pid = fork();
+        if (pid < 0) {
+            error("ERROR in new process creation");
+        }
+        if (pid == 0) {
+
+            //Somebody connected , get his details and print  
+            getpeername(connfd, (SA*)&cli,(socklen_t*)&len); 
+
+            printf("Host connected , ip %s , port %d \n" , 
+                inet_ntoa(cli.sin_addr) , ntohs(cli.sin_port));
+
+            while (TRUE) {
+
+                char temp_dir[100] = "../imgs/";
+
+                //Read Picture Size
+                printf("Reading Picture Size\n");
+                bzero(buff, sizeof(buff));
+
+                long size;
+                if (read(connfd, &size, sizeof(long)) == 0) {
+                    break;
+                }
+
+                // if ((strncmp(buff, "exit", 5)) == 0) { 
+                //     printf("Client Exit...\n"); 
+                //     exit(0); 
+                // }
+
+                // union LongChar myUnion;
+                // myUnion.myByte[0] = buff[0];
+                // myUnion.myByte[1] = buff[1];
+                // myUnion.myByte[2] = buff[2];
+                // myUnion.myByte[3] = buff[3];
+
+
+                // long size = myUnion.mylong;
+                printf("Size: %ld\n", size);
+
+
+                //Read Picture Name
+                printf("Reading Picture Name\n");
+                bzero(buff, sizeof(buff));
+                
+                if (read(connfd, buff, MAX) == 0) {
+                    break;
+                }
+
+                char *token = strtok(buff, "/");
+
+                do {   
+                    name = token;
+                    token = strtok(NULL, "/");
+                } while (token != NULL);
+
+                printf("Name: %s\n", name);
+
+                // break;
+                char counter_str[4];
+                sprintf(counter_str, "%d", counter);
+                strcat(temp_dir, counter_str);
+                strcat(temp_dir, name);
+                strcat(temp_dir,"\0");
+                printf("%s\n",temp_dir);
+                
+                /*
+                //Read Picture Byte Array
+                printf("Reading Picture Byte Array\n");
+                char p_array[size+1];
+                if((nread = read(connfd, p_array, size)) == 0) {
+                    fclose(image);
+                    break;
+                }
+                if (counter < 100) {
+                    image = fopen(temp_dir, "w");
+
+                    if (image == NULL) {
+                        printf("Image didn't open\n");
+                        exit(0);
+                    }
+
+
+                    fwrite(p_array, 1, size, image); 
+                    fclose(image);
+                }*/
+                // long counter = 0L;
+                // while(counter < size) {
+                //     bzero(buff, sizeof(buff));
+                //     ssize_t nread = read(connfd, buff, 256);
+                //     fwrite(buff, 1, nread, image);
+                //     counter += sizeof(buff);
+                // }
+
+                printf("Responding to client\n");
+                bzero(buff, sizeof(buff)); 
+                write(connfd, msg_rcv, MAX);
+                
+                if (counter < 100) {
+                    counter ++;
+                }
+
             }
+            
+            close(connfd);
+        } else {
+            //parent process
+            close(connfd);
+        }
+    }
 
-            // printf("%s\n", trusted);
-            char *client_ip = inet_ntoa(cli.sin_addr);
+    close(sockfd);
 
-            char *istrusted = strstr(trusted, client_ip);
-            char *isnottrusted = strstr(not_trusted, client_ip);
-
-            // printf("%s\n", client_ip);
-
-            if (istrusted != NULL && isnottrusted == NULL) {
-                func(connfd, 0);
-            } else if (istrusted == NULL && isnottrusted != NULL) {
-                func(connfd, 1);
-            } else {
-                printf("Rejected: ip %s\n", client_ip);
-            }
-
-        }    
-        close(sockfd);
+    return 0;
 }
+// int main(int argc, char *argv[]) {
+//     int sockfd, newsockfd, portno;
+//     socklen_t clilen;
+//     char buffer[256];
+//     struct sockaddr_in serv_addr, cli_addr;
+//     int n;
+//     if (argc < 2) {
+//          fprintf(stderr, "ERROR, no port provided\n");
+//          exit(1);
+//     }
+//     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+//     if (sockfd < 0)
+//          error("ERROR opening socket");
+//     bzero((char *) &serv_addr, sizeof(serv_addr));
+//     portno = atoi(argv[1]);
+//     serv_addr.sin_family = AF_INET;
+//     serv_addr.sin_addr.s_addr = INADDR_ANY;
+//     serv_addr.sin_port = htons(portno);
+//     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+//         error("ERROR on binding");
+//     listen(sockfd, 5);
+//     clilen = sizeof(cli_addr);
+//     //Below code is modified to handle multiple clients using fork
+//     //------------------------------------------------------------------
+//     int pid;
+//     while (1) {
+//          newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+//          if (newsockfd < 0)
+//                 error("ERROR on accept");
+//          //fork new process
+//          pid = fork();
+//          if (pid < 0) {
+//               error("ERROR in new process creation");
+//          }
+//          if (pid == 0) {
+//             //child process
+//             close(sockfd);
+//             //do whatever you want
+//             bzero(buffer, 256);
+//             n = read(newsockfd, buffer, 255);
+//             if (n < 0)
+//                 error("ERROR reading from socket");
+//             printf("Here is the message: %s\n", buffer);
+//             n = write(newsockfd, "I got your message", 18);
+//             if (n < 0)
+//                  error("ERROR writing to socket");
+//             close(newsockfd);
+//           } else {
+//              //parent process
+//              close(newsockfd);
+//           }
+//     }
+//     //-------------------------------------------------------------------
+//    return 0;
+// }
