dd5e1cfdab85b123c30fa8a96e2837a39d6dfae7
Ki Sung Lim <kisungqueso@hotmail.com>
2020-11-30 10:20:10 -0600

    [Update] Client for server 3

--- client/src/client.c
+++ client/src/client.c
@@ -245,14 +245,20 @@ int main(int argc, char *argv[]) {
 
 
 	switch (atoi(argv[2])) {
-		case 9090: 
+		case 9090: // Sequential
 			fp = fopen("../../server1/log.json", "w");
 			fprintf(fp, "{\"totalRequests\":%d, \"elapsedTime\":%f, \"pixelCount\":%d}", atoi(argv[5]) * atoi(argv[4]), end-start, width*height*3);
 			fclose(fp);
 			break;
 
-		case 8888:
+		case 8888: // Heavy
 			fp = fopen("../../server2/log.json", "w");
+			fprintf(fp, "{\"totalRequests\":%d, \"elapsedTime\":%f, \"pixelCount\":%d}", atoi(argv[5]) * atoi(argv[4]), end-start, width*height*3);
+			fclose(fp);
+			break;
+
+		case 8070: // Pre-heavy
+			fp = fopen("../../server3/log.json", "w");
 			fprintf(fp, "{\"totalRequests\":%d, \"elapsedTime\":%f, \"pixelCount\":%d}", atoi(argv[5]) * atoi(argv[4]), end-start, width*height*3);
 			fclose(fp);
 			break;
